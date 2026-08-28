cfa88455086f512c63a60ee4d6f9368190cf7970
ashanmahawasala <ashanmahawasala@gmail.com>
2026-08-28 21:33:29 +0530

    validations for incidetn reporting

--- server/src/services/incident.service.js
+++ server/src/services/incident.service.js
@@ -1,33 +1,12 @@
 import * as incidentRepository from "../repositories/incident.repository.js";
+import { validateIncident } from "../validators/incident.validator.js";
 
 export const createIncident = async (incidentData) => {
-  const {
-    category,
-    latitude,
-    longitude,
-    dateTime,
-    description,
-  } = incidentData;
+  validateIncident(incidentData);
 
-  if (
-    !category ||
-    latitude === undefined ||
-    longitude === undefined ||
-    !dateTime ||
-    !description
-  ) {
-    throw new Error("All required fields must be provided");
-  }
-
-  if (latitude < -90 || latitude > 90) {
-    throw new Error("Invalid latitude");
-  }
-
-  if (longitude < -180 || longitude > 180) {
-    throw new Error("Invalid longitude");
-  }
-
-  return await incidentRepository.createIncident(incidentData);
+  return await incidentRepository.createIncident(
+    incidentData
+  );
 };
 
 export const getNearbyIncidents = async () => {
