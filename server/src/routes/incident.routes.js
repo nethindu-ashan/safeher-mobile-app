dd0db63e5372ff86674bc1be6413fb0622c380ad
Oshadha Kalhara <oshadhawedaguruge@gmail.com>
2026-08-30 10:33:11 +0530

    feat: implement safety alert details

--- server/src/routes/incident.routes.js
+++ server/src/routes/incident.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createIncident , getNearbyIncidents  } from "../controllers/incident.controller.js";
+import { createIncident , getNearbyIncidents , getIncidentById  } from "../controllers/incident.controller.js";
 
 const router = express.Router();
 
 router.post("/", createIncident);
 router.get("/nearby", getNearbyIncidents);
+router.get("/:id", getIncidentById);
 
 export default router;
